6ab4f3974c541209e25c331480ab3a8a976a6fea
leeos20k <leeos20k@naver.com>
2023-07-12 14:32:36 +0900

    완료

--- src/users/ViewUser.jsx
+++ src/users/ViewUser.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
 
 const ViewUser = () => {
+
+
+  const { id } = useParams();
+
+  const [user, setUser] = useState({
+    name: '',
+    username: '',
+    email: '',
+  });
+
+  //페이지 시작시 유저데이터를 받아 user에 저장
+  useEffect(() => {
+    loadUser();
+    // eslint-disable-next-line
+  }, []);
+
+  const loadUser = async () => {
+    const result = await axios.get(`http://localhost:8080/users/${id}`);
+    setUser(result.data);
+  };
+
+  const { name, username, email } = user;
+
+
+
+
   return (
     <div className="container">
       <div className="row">
@@ -10,22 +37,22 @@ const ViewUser = () => {
 
           <div className="card">
             <div className="card-header">
-              유저ID : 
+              유저ID : {id}
               <ul className="list-group list-group-flush">
                 <li className="list-group-item">
-                  <b>이름 : </b>
+                  <b>이름 : {name}</b>
                 </li>
                 <li className="list-group-item">
-                  <b>유저네임 : </b>
+                  <b>유저네임 : {username}</b>
                 </li>
                 <li className="list-group-item">
-                  <b>이메일 : </b>
+                  <b>이메일 : {email}</b>
                 </li>
               </ul>
             </div>
           </div>
           <Link className="btn btn-primary my-2" to={'/'}>
-            돌아기기
+            돌아가기
           </Link>
         </div>
       </div>
